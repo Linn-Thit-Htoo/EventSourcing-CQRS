efa11f3d48b2a343a6b4101b5e077e6c910bc254
Linn-Thit-Htoo <lth1212001@gmail.com>
2025-03-05 11:01:56 +0630

    feat: in progress event dispatcher

---
+++ DotNet-EventSourcing.EventDispatcher/Entities/SchemaMigration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet_EventSourcing.EventDispatcher.Entities;
+
+public partial class SchemaMigration
+{
+    public long Version { get; set; }
+
+    public DateTime? InsertedAt { get; set; }
+}
---
+++ DotNet-EventSourcing.EventDispatcher/Resources/MessageResource.cs
@@ -0,0 +1,6 @@
+﻿namespace DotNet_EventSourcing.EventDispatcher.Resources
+{
+    public class MessageResource
+    {
+    }
+}
