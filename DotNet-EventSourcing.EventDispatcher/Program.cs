e1328bf9ff9480bca6925b0e2c6233a7bd4e4508
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-05 23:28:03 +0630

    -

--- DotNet-EventSourcing.EventDispatcher/Program.cs
+++ DotNet-EventSourcing.EventDispatcher/Program.cs
@@ -1,35 +1,34 @@
 using DotNet_EventSourcing.EventDispatcher.Dependencies;
 
-namespace DotNet_EventSourcing.EventDispatcher
+namespace DotNet_EventSourcing.EventDispatcher;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddDependencies(builder);
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddDependencies(builder);
-
-            var app = builder.Build();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseHealthChecks("/health");
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseHealthChecks("/health");
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
