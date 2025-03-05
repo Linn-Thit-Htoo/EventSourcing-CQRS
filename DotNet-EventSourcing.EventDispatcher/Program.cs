77a0149ff1ca47fca85cf7e4cda92f73ec639beb
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-05 23:27:54 +0630

    -

--- DotNet-EventSourcing.EventDispatcher/Program.cs
+++ DotNet-EventSourcing.EventDispatcher/Program.cs
@@ -8,7 +8,6 @@ namespace DotNet_EventSourcing.EventDispatcher
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
