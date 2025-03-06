bfcd1a07e37bd83367056510e7628f54545bce0e
Linn-Thit-Htoo <lth1212001@gmail.com>
2025-03-06 09:54:55 +0630

    feat: added gateway

---
+++ DotNet-EventSourcing.Gateway/Program.cs
@@ -0,0 +1,44 @@
+
+using DotNet_EventSourcing.Gateway.Dependencies;
+using Ocelot.Middleware;
+
+namespace DotNet_EventSourcing.Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDependencies(builder);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseHealthChecks("/health");
+
+            app.UseOcelot();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
