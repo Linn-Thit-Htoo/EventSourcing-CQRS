d5052536313965af2c179a69c3352bebd982cd53
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-06 06:53:24 +0630

    -

--- DotNet-EventSourcing.ProductMicroservice/Features/Product/CreateProduct/CreateProductCommandHandler.cs
+++ DotNet-EventSourcing.ProductMicroservice/Features/Product/CreateProduct/CreateProductCommandHandler.cs
@@ -7,49 +7,48 @@ using DotNet_EventSourcing.ProductMicroservice.Utils;
 using MediatR;
 using Newtonsoft.Json;
 
-namespace DotNet_EventSourcing.ProductMicroservice.Features.Product.CreateProduct
+namespace DotNet_EventSourcing.ProductMicroservice.Features.Product.CreateProduct;
+
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, Result<CreateProductResponse>>
 {
-    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, Result<CreateProductResponse>>
+    private readonly IRepositoryBase<TblProduct> _productRepository;
+    private readonly IBus _bus;
+
+    public CreateProductCommandHandler(IRepositoryBase<TblProduct> productRepository, IBus bus)
     {
-        private readonly IRepositoryBase<TblProduct> _productRepository;
-        private readonly IBus _bus;
+        _productRepository = productRepository;
+        _bus = bus;
+    }
 
-        public CreateProductCommandHandler(IRepositoryBase<TblProduct> productRepository, IBus bus)
+    public async Task<Result<CreateProductResponse>> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        Result<CreateProductResponse> result;
+
+        //bool productDuplicate = await _productRepository
+        //    .GetByCondition(x => x.ProductName.ToLower().Trim().Contains(request.ProductName) && !x.IsDeleted)
+        //    .AnyAsync(cancellationToken);
+        //if (productDuplicate)
+        //{
+        //    result = Result<CreateProductResponse>.Duplicate("Product already exists.");
+        //    goto result;
+        //}
+
+        var entity = request.ToEntity();
+        await _productRepository.AddAsync(entity, cancellationToken);
+        await _productRepository.SaveChangesAsync(cancellationToken);
+
+        DomainEvent productCreatedEvent = new()
         {
-            _productRepository = productRepository;
-            _bus = bus;
-        }
+            AggregateType = "Product",
+            EventType = "ProductCreated",
+            EventData = JsonConvert.SerializeObject(request),
+            StreamId = entity.ProductId
+        };
+        _bus.Send("DirectExchange", "EventQueue", "eventdirect", productCreatedEvent);
 
-        public async Task<Result<CreateProductResponse>> Handle(CreateProductCommand request, CancellationToken cancellationToken)
-        {
-            Result<CreateProductResponse> result;
+        result = Result<CreateProductResponse>.Success("Saving Product Successful.");
 
-            //bool productDuplicate = await _productRepository
-            //    .GetByCondition(x => x.ProductName.ToLower().Trim().Contains(request.ProductName) && !x.IsDeleted)
-            //    .AnyAsync(cancellationToken);
-            //if (productDuplicate)
-            //{
-            //    result = Result<CreateProductResponse>.Duplicate("Product already exists.");
-            //    goto result;
-            //}
-
-            var entity = request.ToEntity();
-            await _productRepository.AddAsync(entity, cancellationToken);
-            await _productRepository.SaveChangesAsync(cancellationToken);
-
-            DomainEvent productCreatedEvent = new()
-            {
-                AggregateType = "Product",
-                EventType = "ProductCreated",
-                EventData = JsonConvert.SerializeObject(request),
-                StreamId = entity.ProductId
-            };
-            _bus.Send("DirectExchange", "EventQueue", "eventdirect", productCreatedEvent);
-
-            result = Result<CreateProductResponse>.Success("Saving Product Successful.");
-
-            result:
-            return result;
-        }
+        result:
+        return result;
     }
 }
