3ed55fd5e08dafcd0e9dc0de4ed73dfdb9ea3f37
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-06 06:52:56 +0630

    -

--- DotNet-EventSourcing.ProductMicroservice/Features/Product/Core/ProductController.cs
+++ DotNet-EventSourcing.ProductMicroservice/Features/Product/Core/ProductController.cs
@@ -4,31 +4,30 @@ using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet_EventSourcing.ProductMicroservice.Features.Product.Core
+namespace DotNet_EventSourcing.ProductMicroservice.Features.Product.Core;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductController : ControllerBase
+    private readonly ISender _sender;
+
+    public ProductController(ISender sender)
     {
-        private readonly ISender _sender;
+        _sender = sender;
+    }
 
-        public ProductController(ISender sender)
-        {
-            _sender = sender;
-        }
+    [HttpPost("CreateProduct")]
+    public async Task<IActionResult> CreateProductAsync(CreateProductCommand command, CancellationToken cs)
+    {
+        var result = await _sender.Send(command, cs);
+        return Ok(result);
+    }
 
-        [HttpPost("CreateProduct")]
-        public async Task<IActionResult> CreateProductAsync(CreateProductCommand command, CancellationToken cs)
-        {
-            var result = await _sender.Send(command, cs);
-            return Ok(result);
-        }
-
-        [HttpPut("UpdateBlog")]
-        public async Task<IActionResult> UpdateBlogAsync(UpdateProductCommand command, CancellationToken cs)
-        {
-            var result = await _sender.Send(command, cs);
-            return Ok(result);
-        }
+    [HttpPut("UpdateBlog")]
+    public async Task<IActionResult> UpdateBlogAsync(UpdateProductCommand command, CancellationToken cs)
+    {
+        var result = await _sender.Send(command, cs);
+        return Ok(result);
     }
 }
