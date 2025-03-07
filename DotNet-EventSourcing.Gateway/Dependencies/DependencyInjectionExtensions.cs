9399dc32d10b86b8fe6ebea5fee31167466e2102
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-07 06:42:46 +0630

    -

--- DotNet-EventSourcing.Gateway/Dependencies/DependencyInjectionExtensions.cs
+++ DotNet-EventSourcing.Gateway/Dependencies/DependencyInjectionExtensions.cs
@@ -1,25 +1,24 @@
 ﻿using Ocelot.DependencyInjection;
 
-namespace DotNet_EventSourcing.Gateway.Dependencies
+namespace DotNet_EventSourcing.Gateway.Dependencies;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+        builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        builder.Services.AddControllers().AddJsonOptions(opt =>
         {
-            builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables();
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
 
-            builder.Services.AddControllers().AddJsonOptions(opt =>
-            {
-                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-            });
+        builder.Services.AddOcelot();
+        builder.Services.AddHealthChecks();
 
-            builder.Services.AddOcelot();
-            builder.Services.AddHealthChecks();
-
-            return services;
-        }
+        return services;
     }
 }
