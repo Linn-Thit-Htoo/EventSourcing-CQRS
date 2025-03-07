b0b984023b2bd90791dc2e745fd57b469a979a68
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-07 06:43:59 +0630

    -

--- DotNet-EventSourcing.EventDispatcher/Dependencies/DependencyInjectionExtensions.cs
+++ DotNet-EventSourcing.EventDispatcher/Dependencies/DependencyInjectionExtensions.cs
@@ -1,12 +1,4 @@
-﻿global using DotNet_EventSourcing.EventDispatcher.Configurations;
-global using DotNet_EventSourcing.EventDispatcher.Entities;
-global using DotNet_EventSourcing.EventDispatcher.Persistence.Base;
-global using DotNet_EventSourcing.EventDispatcher.Services.RabbitMQ;
-global using DotNet_EventSourcing.EventDispatcher.Utils;
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.EntityFrameworkCore;
-
-namespace DotNet_EventSourcing.EventDispatcher.Dependencies;
+﻿namespace DotNet_EventSourcing.EventDispatcher.Dependencies;
 
 public static class DependencyInjectionExtensions
 {
--- DotNet-EventSourcing.EventDispatcher/GlobalUsings.cs
+++ DotNet-EventSourcing.EventDispatcher/GlobalUsings.cs
@@ -1,1 +1,7 @@
-﻿
+﻿global using DotNet_EventSourcing.EventDispatcher.Configurations;
+global using DotNet_EventSourcing.EventDispatcher.Entities;
+global using DotNet_EventSourcing.EventDispatcher.Persistence.Base;
+global using DotNet_EventSourcing.EventDispatcher.Services.RabbitMQ;
+global using DotNet_EventSourcing.EventDispatcher.Utils;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
